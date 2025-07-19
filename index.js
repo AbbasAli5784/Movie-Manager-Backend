97785c8d734ca5ababeb1be5b32d041f4e7a4862
AbshaarH <156251353+AbshaarH@users.noreply.github.com>
2025-07-18 22:04:37 -0400

    add movie routes and mongodb setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const routeExample = require("./src/routes/route");
+const movieRoutes = require("./src/routes/movies");
 
 dotenv.config();
 
@@ -17,7 +17,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use("/api/example", routeExample);
+app.use("/api/movies", movieRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
